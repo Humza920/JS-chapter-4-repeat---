f704769395d773c956be679e50b6b5e87d16ffb5
Humza920 <humza.rehman.779@gmail.com>
2024-04-06 13:59:50 -0700

    NOT YET

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 // var me = "Humza" , you = "Bilal" , he = "Ashar";
 // console.log(me,he,you);
 // IS MAI 3 VARIABLE IK SATH DECLARE HUE WAI HAI YANI HUM IK STATMENT MAI MULTIPLE VARIABLES DECLARE KAR SKTAI HAI 
+
+// QUESTION 2
+// illegal var declare
+// var if
+// var 20hum
+// var do-it
+// var full name
+// var last.name
+// legal var declare
+// var fullName  
+// var $book 
+// var human20
+// var c_V
+// var tw$ice
