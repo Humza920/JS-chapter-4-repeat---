10569ccf9a8ff3ffd13dce768fed1d24b22d3dee
Humza920 <humza.rehman.779@gmail.com>
2024-04-06 14:13:32 -0700

    LITTLE BIT

--- app.js
+++ app.js
@@ -18,3 +18,30 @@
 // var human20
 // var c_V
 // var tw$ice
+
+// QUESTION 3
+const ul = document.querySelector("ul")
+
+const dollar = "$"
+
+const underScore = "_"
+
+const words = "leters"
+
+const num = "numbers"
+
+ul.innerHTML = 
+`<h1>Rules for naming JS variables</h1>
+
+<li>A heading stating “Rules for naming JS variables”</li> 
+
+<li>Variable names can only contain ${dollar}, ${underScore},
+${words} and ${num}.
+For example $my_1stVariable</li> 
+
+<li>Variables must begin with a ${dollar}, ${underScore} or
+${words}. For example $name, _name or name</li>
+
+<li></li> 
+
+<li></li>`
